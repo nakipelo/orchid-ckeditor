e8e816d1381634b5f6d8a07361c70b87aa379c0a
Egor Gruzdev <egorgruzdev@gmail.com>
2023-06-18 21:22:49 +0200

    возможность передать свои options для конкретного редактора, также сделать merge опций

--- src/CKEditor.php
+++ src/CKEditor.php
@@ -7,15 +7,32 @@ use Orchid\Screen\Field;
 
 class CKEditor extends Field
 {
-    /** @var string */
-    protected $view = 'ckeditor::field';
+	/** @var string */
+	protected $view = 'ckeditor::field';
 
-    /** @var array  */
-    protected $attributes = [
-    ];
+	/** @var array  */
+	protected $attributes = [
+		'options' => [],
+	];
 
-    public static function make(?string $name = null): Field
-    {
-        return (new static())->name($name);
-    }
+	public static function make(?string $name = null): CKEditor
+	{
+		return (new static())
+			->name($name)
+			->setOptions(config('ckeditor.options', []));
+	}
+
+	public function setOptions(array $options): CKEditor
+	{
+		$this->attributes['options'] = $options;
+
+		return $this;
+	}
+
+	public function mergeOptions(array $options): CKEditor
+	{
+		$this->attributes['options'] = array_merge($this->attributes['options'], $options);
+
+		return $this;
+	}
 }
--- views/field.blade.php
+++ views/field.blade.php
@@ -1,17 +1,17 @@
 @component($typeForm, get_defined_vars())
-<div data-controller="ckeditor"
-     data-ckeditor-id-value="{{ $id }}"
-     data-ckeditor-options-value="{{ json_encode(config('ckeditor.options')) }}"
-     data-ckeditor-editor-url-value="{{ config('ckeditor.editorUrl') }}"
->
-    <div data-ckeditor-target="editor"></div>
+    <div data-controller="ckeditor"
+         data-ckeditor-id-value="{{ $id }}"
+         data-ckeditor-options-value="{{ json_encode($options, flags: JSON_FORCE_OBJECT) }}"
+         data-ckeditor-editor-url-value="{{ config('ckeditor.editorUrl') }}"
+    >
+        <div data-ckeditor-target="editor"></div>
 
-    <input id="{{ $id }}"
-           data-ckeditor-target="input"
-           name="{{ $name }}"
-           type="hidden"
-           class="form-control"
-           value="{{ $value ?? '' }}"
-    />
-</div>
+        <input id="{{ $id }}"
+               data-ckeditor-target="input"
+               name="{{ $name }}"
+               type="hidden"
+               class="form-control"
+               value="{{ $value ?? '' }}"
+        />
+    </div>
 @endcomponent
