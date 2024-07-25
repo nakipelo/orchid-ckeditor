376850fe3517c89f3a6508fe9790b8a3b87890f6
Юрий <y.grevtsov@tapir.ws>
2024-07-25 17:14:38 +0400

    resolve v

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ return [
      * Скрипты загрузки редактора
      * @link https://ckeditor.com/ckeditor-4/download/
      */
-    'editorUrl' => '//cdn.ckeditor.com/4.16.2/full/ckeditor.js',
+    'editorUrl' => '//cdn.ckeditor.com/4.24.0/full/ckeditor.js',
 
     /**
      * Настройки редактора по-умолчанию
